fdd93d4bc7dc34e99a0c5a894414c280c714949b
Alexander Ostach <ukrorion@gmail.com>
2016-07-12 19:17:45 +0300

    added toggle action to checkbox
    fixed issue with map method

--- app/assets/javascripts/todo_redux/components/item.js
+++ app/assets/javascripts/todo_redux/components/item.js
@@ -1,8 +1,18 @@
+var store = appRedux.store;
+
 appRedux.Item = React.createClass({
+  handleChecked: function(){
+    store.dispatch({
+      type: 'TOGGLE_TASK',
+      id: this.props.id,
+      status: this.props.status,
+      description: this.props.children
+    });
+  },
   render: function() {
     return (
       <li className="listItem" >
-        <input name="status" type="checkbox" checked={this.props.status} readOnly={true}/>
+        <input name="status" type="checkbox" checked={this.props.status} onChange={this.handleChecked} />
         {this.props.children}
       </li>
     );
--- app/assets/javascripts/todo_redux/reducers/tasks.js
+++ app/assets/javascripts/todo_redux/reducers/tasks.js
@@ -1,14 +1,30 @@
+appRedux.Task = function(state, action){
+  switch(action.type){
+    case 'ADD_TASK':
+      return {
+        id: action.id,
+        description: action.description,
+        status: action.status
+      };
+    case 'TOGGLE_TASK':
+      if(state.id !== action.id)
+        return state;
+      return _.assign({}, state, {status: !state.status});
+    default:
+      return state;
+  }
+}
+var Task = appRedux.Task;
+
 appRedux.Tasks = function(state, action){
   state = (typeof state == "undefined" ? [] : state);
   switch(action.type){
     case 'ADD_TASK':
-      return _.concat(state, [
-          {
-            id: action.id,
-            description: action.description,
-            status: action.status
-          }
-        ]);
+      return _.concat(state, [Task(undefined, action)]);
+    case 'TOGGLE_TASK':
+      return state.map(function(st){
+        return Task(st,action);
+      })
     default:
       return state;
   }
