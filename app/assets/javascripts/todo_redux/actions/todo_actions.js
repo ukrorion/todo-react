f385af55094c9bd515e781a3a72ff49a11dc27a1
Alexander Ostach <ukrorion@gmail.com>
2016-07-13 23:52:38 +0300

    refactoring of actions object

--- app/assets/javascripts/todo_redux/actions/todo_actions.js
+++ app/assets/javascripts/todo_redux/actions/todo_actions.js
@@ -1,14 +1,17 @@
 var store = appRedux.store;
 
-appRedux.BoxActions = {
-  fetch_tasks: function(self) {
-    $.ajax({
-      url: self.props.url,
+var send_data = function(url, request_type, action_name, data){
+  data = data || {};
+  
+  return  $.ajax({
+      url: url,
+      type: request_type,
       dataType: 'json',
+      data: data,
       cache: false,
       success: function(data){
         store.dispatch({
-          type: 'FETCH_TASKS',
+          type: action_name,
           data: data
         });
       },
@@ -16,51 +19,21 @@ appRedux.BoxActions = {
         console.log(error);
       }
     });
+}
+
+
+
+appRedux.BoxActions = {
+  fetch_tasks: function(self) {
+    send_data(self.props.url, 'get', 'FETCH_TASKS');
   },
   add_task: function(description){
-    $.ajax({
-      url: '/tasks',
-      type: 'post',
-      dataType: 'json',
-      data: {'task' : {'description' : description}},
-      success: function(data){
-        store.dispatch({type: 'ADD_TASK', data: data});
-      },
-      error: function(error){
-        console.log(error);
-      }
-    });
+    send_data('/tasks', 'post', 'ADD_TASK', {'task' : {'description' : description}});
   },
-  toggle_task: function(_this) {
-    $.ajax({
-      url: '/tasks/' + _this.props.id,
-      type: 'put',
-      dataType: 'json',
-      success: function(data){
-        store.dispatch({
-          type: 'TOGGLE_TASK',
-          data: data
-        });
-      },
-      error: function(error){
-        console.log(error)
-      }
-    });
+  toggle_task: function(self) {
+    send_data('/tasks/' + self.props.id, 'put', 'TOGGLE_TASK');
   },
-  delete_task: function(_this) {
-    $.ajax({
-      url: '/tasks/'+_this.props.id,
-      type: 'delete',
-      dataType: 'json',
-      success: function(data){
-        store.dispatch({
-          type: 'DELETE_TASK',
-          data: data
-        });
-      },
-      error: function(error){
-        console.log(error);
-      }
-    });
+  delete_task: function(self) {
+    send_data('/tasks/' + self.props.id, 'delete', 'DELETE_TASK');
   }
 }
